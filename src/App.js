1e9c24136242388f915251a8bc3f547a17b061c4
Andrea <andrea.soriano@outlook.com>
2024-07-02 16:37:05 +0800

    created react app with bootstrap npm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>code pls work now</p>
+        <p>WEATHER TIME</p>
       </header>
+      <button class="btn btn-primary" type="submit">
+        wow a button
+      </button>
     </div>
   );
 }
-
-export default App;
